59dd57c6b9cd9b76031f44ef47df8f2da4213003
Agnia <delirium_da@mail.ru>
2016-06-11 02:04:04 +0600

    main function

--- kvadrat.c
+++ kvadrat.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-double diskr(float a, float b, float c)
-{
-	float d;
-	d = b * b - 4 * a * c;
-	if (d < 0)
-		printf ("no roots");
-	else
-	{
-		if (d == 0)
-			printf ("x = %g ", - b / (2 * a));
-		else
-			printf ("x1 = %g, x2 = %g", (- b + sqrt(d)) / (2 * a), (- b - sqrt(d))/(2 * a));
-	}
-}
-
 int main()
 {
 	float a, b, c;
